5b32c119896d3c1ef85124b30787a9a674ef51dd
Suvorov S.K <s.k.suvorov.email@gmail.com>
2021-12-27 03:25:13 +0300

    -Добавлены турели

---
+++ Assets/Scripts/Systems/TurretRottationSystem.cs
@@ -0,0 +1,33 @@
+﻿using Leopotam.EcsLite;
+using UnityEngine;
+
+public class TurretRottationSystem :  IEcsRunSystem
+{
+    private EcsWorld _ecsWorld;
+
+    public void Run(EcsSystems systems)
+    {
+        _ecsWorld = systems.GetWorld();
+        var playerFilter = _ecsWorld.Filter<Player>().End();
+        var playerPool = _ecsWorld.GetPool<Player>();
+        var turretFilter = _ecsWorld.Filter<Turret>().End();
+        var turretPool = _ecsWorld.GetPool<Turret>();
+
+        foreach(var playerEntity in playerFilter)
+        {
+            ref var player = ref playerPool.Get(playerEntity);
+
+            foreach( var turretEntity in turretFilter)
+            {
+                ref var turret = ref turretPool.Get(turretEntity);
+
+                Vector3 lookPos = player.PlayerTransform.position - turret.TurretTransform.position;
+                Vector3 rotation = Quaternion.Lerp(turret.TurretTransform.rotation, Quaternion.LookRotation(lookPos), Time.deltaTime).eulerAngles;
+                rotation.z = 0;
+                rotation.x = 0;
+
+                turret.TurretTransform.eulerAngles = rotation;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UnityComponents/TurretView.cs
@@ -0,0 +1,17 @@
+﻿using Leopotam.EcsLite;
+using UnityEngine;
+
+public class TurretView : MonoBehaviour
+{
+    public int TurretWeapon;
+    public EcsWorld EcsWorld;
+    public int ShootingDistance;
+    public void Shoot()
+    {
+        EcsWorld.GetPool<Shoot>().Add(TurretWeapon);
+    }
+    public void Reload()
+    {
+        EcsWorld.GetPool<ReloadingFinished>().Add(TurretWeapon);
+    }
+}
